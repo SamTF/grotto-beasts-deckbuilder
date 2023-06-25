2cfadcab5219d68502c7c1acdcbb4cbcf6cc598d
SamTF <elseban88@gmail.com>
2023-06-25 19:43:51 +0200

    Created header component

---
+++ web-app/src/components/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Navbar from "./Navbar.svelte";
+</script>
+
+<header>
+    <div class="center">
+        <a href="/">
+            <img src="/images/Logo.webp" alt="Grotto Beasts Logo">
+        </a>
+    </div>
+
+    <Navbar />
+
+    <div>
+        <nav>
+            <a href="/account">Account</a>
+        </nav>
+    </div>
+</header>
