ebafcb2a5a0db177381a51a98551384724f8bb24
SamTF <elseban88@gmail.com>
2023-06-25 19:40:39 +0200

    Placeholder main page

--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to my grotto beasts deck builder</h1>
+<p>Placeholder main page</p>
