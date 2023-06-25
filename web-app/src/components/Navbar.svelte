fcdb618db255b1d8866cdb1faab7966e0a0e4420
SamTF <elseban88@gmail.com>
2023-06-25 19:44:13 +0200

    Created placeholder navbar

---
+++ web-app/src/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<!-- JS -->
+<script></script>
+
+<!-- HTML -->
+<nav>
+    <a href="/">Home</a>
+    <a href="/decks">Cards</a>
+    <a href="/decks">Decks</a>
+    <a href="/decks">Your Decks</a>
+    <a href="/settings">Settings</a>
+</nav>
