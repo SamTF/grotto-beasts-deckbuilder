5a49bff12772f1a4648685ec1142ee2f3c542017
SamTF <elseban88@gmail.com>
2023-06-25 19:44:49 +0200

    Created deck stats bar component

---
+++ web-app/src/components/DeckStatsBar.svelte
@@ -0,0 +1,30 @@
+<!-- JS -->
+<script>
+    import { CardTypes, cardsByType, countCardType } from "$lib/cardUtils"
+    
+    // Props
+    export let deck = []
+
+    // Vars
+    let totalCards = deck.length
+    let challengerNum = countCardType(deck, CardTypes.CHALLENGER)
+    let beastNum = countCardType(deck, CardTypes.BEAST)
+    let grottoNum = countCardType(deck, CardTypes.GROTTO)
+    let wishNum = countCardType(deck, CardTypes.WISH)
+</script>
+
+<!-- HTML -->
+<div class="deck-stats-bar">
+    <!-- left panel contaning deck general info -->
+    <div class="left">
+        <span>{totalCards} total cards</span>
+    </div>
+
+    <!-- right panel containing card count by type -->
+    <div class="right">
+        <span data-card-type="challenger">{challengerNum}</span>
+        <span data-card-type="beast">{beastNum}</span>
+        <span data-card-type="grotto">{grottoNum}</span>
+        <span data-card-type="wish">{wishNum}</span>
+    </div>
+</div>
