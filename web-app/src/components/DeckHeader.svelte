63dc21798868252bcaab488f8d5cab97cb03f0a3
SamTF <elseban88@gmail.com>
2023-06-25 19:44:41 +0200

    Created deck header component

---
+++ web-app/src/components/DeckHeader.svelte
@@ -0,0 +1,22 @@
+<!-- JS -->
+<script>
+    export let author, name, tags
+</script>
+
+<!-- HTML -->
+<div class="deck-header">
+    <div class="info-container">
+        <a href={`/users/${author}`} class="deck-author">{author}</a>
+        <h1 class="deck-name">{name}</h1>
+        
+        <ul class="deck-tags">
+            <li>Dice Rolling</li>
+            <li>Coin Flipping</li>
+        </ul>
+    </div>
+</div>
+
+<div class="header-divider">
+
+</div>
+
