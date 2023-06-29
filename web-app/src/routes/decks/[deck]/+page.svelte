171494a85d7006faefe14e141f8ecd97bfe8d983
SamTF <elseban88@gmail.com>
2023-06-29 20:05:41 +0200

    Removed unnecessary code and prints

--- web-app/src/routes/decks/[deck]/+page.svelte
+++ web-app/src/routes/decks/[deck]/+page.svelte
@@ -6,48 +6,18 @@
     import DeckStatsBar from "$components/DeckStatsBar.svelte";
 
     //  Local Data
-    import { deck } from '$lib/test_deck.js'
+    // import { deck } from '$lib/test_deck.js'
     // console.log(deck)
 
+    // Receive props from API
     export let data
-    // console.log(data)
 
     const cards = data.deck.cards_json
-    // console.log(cards)
-
-
-
-    const totalCards = deck.reduce((total, item) => total + item.quantity, 0)
-    const totalBeasts = countCardType(deck, 'Beast')
-    // const totalBeasts = deck.filter(x => x.type == 'Beast').reduce((total, item) => total + item.quantity, 0)
-
-    // Events
-    const onCardHover = event => {
-        console.log("PARENT HOVER EVENT DETECTED")
-        console.log(event.detail)
-    }
-
+    const deck = cards.deck
 </script>
 
 <!-- HTML -->
-<DeckHeader name="Super Luck" author="Jerma" tags="cool" />
-
-
-<!-- <h1>deck {data.deck.name}</h1> -->
-
-<!-- <h3>JSON</h3>
-{#each Object.keys(cards) as card}
-    <p>{card}: {cards[card]}</p>
-{/each}
-
-
-
-<h3>Local Data Test</h3>
-{#each deck as card}
-    <p>{card.name} | {card.type} | {card.quantity}</p>
-{/each} -->
-
-
+<DeckHeader name={data.deck.name} author={data.deck.expand.author.name} tags="cool" />
 
 <!-- DECK -->
 <div class="center">
@@ -56,8 +26,3 @@
 
 <!-- Stats Bar -->
 <DeckStatsBar {deck} />
-
-<!-- <h3>Relational</h3>
-{#each data.deck.expand.cards_relational as c}
-    <p>{c.name}</p>
-{/each} -->
