6ed00b6a6a6541db5e4fcffaadc72f958ac11af0
SamTF <elseban88@gmail.com>
2023-06-29 20:07:29 +0200

    Grid showcasing all created decks

--- web-app/src/routes/decks/+page.svelte
+++ web-app/src/routes/decks/+page.svelte
@@ -1,12 +1,44 @@
 <!-- JS -->
 <script>
+    // Imports
+    import DeckGridItem from '$components/DeckExplorer/DeckGridItem.svelte';
+
     export let data
 
     console.log(data)
-    console.log(data.url.searchParams)
+    // console.log(data.url.searchParams)
 </script>
 
 <!-- HTML -->
-<h1>Decks Explorer</h1>
-<p>{data.deck}</p>
-<p>This page will be a browser/galary for all decks created by users.</p>
+<div class="header-divider" style="height: 6rem; margin-bottom: 0;"></div>
+
+<div class="deck-explorer-container">
+    <div class="explorer-filters">
+        <h1>Grotto Explorer!</h1>
+
+        <div class="deck-types">
+            <ul class="btn-tags">
+                <li class="btn-tag">All Decks</li>
+                <li class="btn-tag">Official Decks</li>
+                <li class="btn-tag">Your Decks</li>
+                <li class="btn-tag">Bookmarked Decks</li>
+            </ul>
+        </div>
+
+        <div class="deck-search">
+            <button>Filters</button>
+            <input type="text" name="deck-search" id="deck-search" placeholder="Search decks...">
+        </div>
+    </div>
+
+    <!-- DECK GRID -->
+    <div class="deck-grid">
+        
+            {#each data.decks as deck}
+                <DeckGridItem {deck} />
+            {/each}
+        
+        
+    </div>
+
+</div>
