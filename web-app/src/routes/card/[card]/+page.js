56d816f83e50c2ad5f28c7b36172582a70829905
SamTF <elseban88@gmail.com>
2023-06-30 01:20:18 +0200

    Fetch a card on pb via its name OR number

---
+++ web-app/src/routes/card/[card]/+page.js
@@ -0,0 +1,29 @@
+import { pb } from '$lib/pocketbase.js'
+import { json, error, redirect } from "@sveltejs/kit";
+
+export async function load({ url, params}) {
+    console.log(params.card)
+
+    // the filter by which to find the desired card
+    let filter = `name ~ "${params.card}"`
+    
+    // filter by card number instead of name if a number was given in the URL parameter
+    const usingId = Number.isInteger(parseInt(Number(params.card)))
+    if (usingId) {
+        const id = parseInt(Number(params.card))
+        filter = `number = ${id}`
+    }
+
+    const results = await pb.collection("cards").getList(1, 1, {
+        filter: filter
+    })
+
+    // if no matching card was found, redirect to glueman
+    if (results.items.length < 1) {
+        throw redirect(303, '/card/glueman')
+    }
+
+    // if a card was found successfully
+    const card = results.items[0]
+    return { card }
+}
