8338361a312d767cc82ea25ec57aa47436f2572c
SamTF <elseban88@gmail.com>
2023-06-29 20:06:23 +0200

    Created component for Deck items in the Deck Explorer grid

---
+++ web-app/src/components/DeckExplorer/DeckGridItem.svelte
@@ -0,0 +1,16 @@
+<!-- These are the Deck items that populate the Deck Explorer -->
+
+<!-- JS -->
+<script>
+    export let deck
+
+    let challengerImg = `${deck.expand.challenger.number}. ${deck.expand.challenger.name}.webp`
+    console.log(challengerImg)
+</script>
+
+<!-- HTML -->
+<a class="deck-grid-item" href={`/decks/${deck.id}`} style={`--bg-image: url("images/cards/360/${challengerImg}")`}>
+    <h2 class="deck-item-name">{deck.name}</h2>
+    <p class="deck-item-author">{deck.expand.author.name}</p>
+    <p class="deck-item-info">{deck.expand.challenger.name}</p>
+</a>
