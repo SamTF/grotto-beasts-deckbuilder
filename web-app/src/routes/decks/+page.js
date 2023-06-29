e8cb94227669f0a356f2430e6bd96360d8cb7a1b
SamTF <elseban88@gmail.com>
2023-06-29 20:06:59 +0200

    Fetch list of all created decks

--- web-app/src/routes/decks/+page.js
+++ web-app/src/routes/decks/+page.js
@@ -1,6 +1,15 @@
 import { pb } from '$lib/pocketbase.js'
 
 export async function load({ url, params, route, fetch }) {
+    const result = await pb.collection("decks").getList(1, 50, {
+        sort: 'created',
+        expand: 'challenger, author'
+    });
+    
+    let decks = result.items
+
+    return { decks: decks }
+
     return {
         deck: "grotto beats",
         url: url,
