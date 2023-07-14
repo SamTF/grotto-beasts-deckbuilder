eb1d6c46e17c438ab779a99d66f59038a38a4b73
SamTF <elseban88@gmail.com>
2023-07-14 13:10:52 +0200

    Created API route to fetch card info as JSON

---
+++ web-app/src/routes/api/card/[card]/+server.js
@@ -0,0 +1,28 @@
+import { pb } from '$lib/pocketbase.js';
+import { json } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+
+const collection = "cards"
+
+export async function GET({ url, params }) {
+    // console.log(url)
+    // console.log(params)
+
+    const id = params.card
+
+    // filter by card number instead of name if a number was given in the URL parameter
+    const usingId = Number.isInteger(parseInt(Number(params.card)))
+
+    const result = await pb.collection(collection).getList(1,1, {
+        filter: usingId ? `number = ${id}` : `name ~ "${params.card}"`,
+        '$autoCancel' : false
+    })
+
+    // check
+    if (result.items.length < 1) {
+        throw error(400, `card [${id}] not found :c`)
+    }
+
+    const card = result.items[0]
+    return json(card)
+}
