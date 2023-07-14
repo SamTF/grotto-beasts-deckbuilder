d77c744388d6347ef0740c15e3ca6af35af0006c
SamTF <elseban88@gmail.com>
2023-07-14 19:06:57 +0200

    Expand card info on load

--- web-app/src/routes/decks/[deck]/+page.js
+++ web-app/src/routes/decks/[deck]/+page.js
@@ -5,16 +5,33 @@ export async function load({ url, params}) {
     console.log(`URL SLUG : ${params.deck}`)
 
     // Load Deck data if a deck exists with the given ID
+    let deck = null
     try {
         const result = await pb.collection("decks").getOne(params.deck, {
             expand: 'challenger, author'
         })
 
-        return { deck: result }
+        deck = result
     }
-    
-    // if no deck was found with the given ID, redirect to deck explorer
+    // If no deck was found with the given ID, redirect to deck explorer
     catch (error) {
-        throw redirect(303, '/decks')
+        throw redirect(303, '/decks')        
     }
+
+    // Expand card info
+    let fullCards = []
+    for (let i = 0; i < deck.cards_json.deck.length; i++) {
+        const element = deck.cards_json.deck[i];
+        const id = element.id;
+        const res = await fetch(`${url.origin}/api/card/${id}`)
+        const item = await res.json()
+        
+        // add card quantity field
+        item.quantity = element.quantity
+        console.log(item)
+        fullCards.push(item)
+    }    
+
+    // Return simplified deck and full expanded cards
+    return { deck: deck, fullCards: fullCards }
 }
