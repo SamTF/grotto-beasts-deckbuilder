e7046926c572707c50082d05887a807823e0eb72
SamTF <elseban88@gmail.com>
2023-07-14 13:10:35 +0200

    created function to calculate mana curve

--- web-app/src/lib/cardUtils.js
+++ web-app/src/lib/cardUtils.js
@@ -16,3 +16,42 @@ export const cardsByType = (deck, type) => {
 export const countCardType = (deck, type) => {
     return deck.filter(x => x.type == type).reduce((total, item) => total + item.quantity, 0)
 }
+
+// Create a deck from the given cards and their respective quantity
+export const populateDeck = (cards) => {
+    let deck = []
+
+    // loop over all cards and append them to the deck according to their quantity
+    cards.forEach(c => {
+        // ignore challengers
+        if (c.type == "Challenger") {
+            return
+        }
+
+        // append cards to deck
+        for (let i = 0; i < c.quantity; i++) {
+            deck.push(c)
+        }
+    })
+
+    // return the populated deck
+    return deck
+}
+
+// Counts the number of cards at each mana cost
+const GetManaCurve = (fullCards) => {
+    let manaCurve = {}
+
+    fullCards.forEach(card => {
+        if (card.type == 'Challenger') return
+
+        if (card.cost in manaCurve) {
+            manaCurve[card.cost] += card.quantity
+        } else {
+            manaCurve[card.cost] = card.quantity
+        }
+        
+    });
+
+    return manaCurve
+}
