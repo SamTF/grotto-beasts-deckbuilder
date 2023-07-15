ca1ffdcfcebae48d3f705ae1b9843f0aff500bd7
SamTF <elseban88@gmail.com>
2023-07-15 18:45:51 +0200

    Simple Toggle button component

---
+++ web-app/src/components/BtnToggle.svelte
@@ -0,0 +1,18 @@
+<!-- JS -->
+<script>
+    // Props
+    export let text = 'Toggle Button'
+    export let onClick = () => {console.log("click")}
+    export let toggle = false
+    export let cardType
+</script>
+
+<!-- HTML -->
+<button
+    class="btn-toggle"
+    class:active="{toggle}"
+    data-card-type={cardType}
+    on:click={() => {toggle = !toggle}}
+>   
+    {text}
+</button>
