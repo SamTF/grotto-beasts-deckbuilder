3d3bd61dcbfcf018a6e0e8550907b4d2153d0be2
SamTF <elseban88@gmail.com>
2023-07-15 18:45:29 +0200

    Simple card component to display in the explorer grid

---
+++ web-app/src/components/Cards/Card.svelte
@@ -0,0 +1,14 @@
+<!-- A Grotto Beast card as displayed in the card grid -->
+
+<!-- JS -->
+<script>
+    // Props
+    export let card
+</script>
+
+<!-- HTML -->
+<div class="card" id={card.number}>
+    <div class="card-image">
+        <a href={`/card/${card.number}`}><img src={`/images/cards/360/${card.number}. ${card.name}.webp`} alt={card.name} loading="lazy"></a>
+    </div> 
+</div>
