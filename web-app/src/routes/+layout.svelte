0073963c144fbc0aa11108cb25b49d0a2a68b5a8
SamTF <elseban88@gmail.com>
2023-07-23 22:07:32 +0200

    Referencing the decklist store so that it persists (maybe?)

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 <script>
     import Header from "$components/UI/Header.svelte";
     import Footer from "$components/UI/Footer.svelte";
+    import { decklistAdvance } from '$lib/stores/decklist'
+
+    const x = $decklistAdvance
+    console.log(x)
 </script>
 
 <Header />
