2ab4cb5fd3d6e365bd41dc28add1c7f655940d57
SamTF <elseban88@gmail.com>
2023-07-23 22:08:17 +0200

    Item component for each card in the Decklist Sidebar

---
+++ web-app/src/components/DeckBuilder/DecklistItem.svelte
@@ -0,0 +1,37 @@
+<!-- A Card item in the decklist sidebar -->
+
+<!-- JS -->
+<script>
+    // Imports
+    import { decklistAdvance } from '$lib/stores/decklist'
+
+    // Props
+    export let card
+</script>
+
+<!-- HTML -->
+<!-- <a href={`/card/${number}`} target="_blank"> -->
+<div class="decklist-item" data-card-type={card.type} on:click={() => decklistAdvance.remove(card)}  on:contextmenu={() => decklistAdvance.remove(card)} on:keypress={() => {}}>
+    <!-- Quantity in Deck -->
+    <div class="quantity">
+        <span>{card.quantity}x</span>
+    </div>
+
+    <!-- Card Name -->
+    <div class="name">
+        <span>{card.name}</span>
+    </div>
+
+    {#if card.type == 'Challenger'}
+        <div class="goal">
+            <span>{card.goal}</span>
+        </div>
+    {:else}
+        <!-- Summoning Cost -->
+        <div class="cost">
+            <span>{card.cost}</span>
+        </div>
+    {/if}
+    
+</div>
+<!-- </a> -->
