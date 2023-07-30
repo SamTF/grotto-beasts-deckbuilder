3a05438bb25151489d84b94183d2845283e1b945
SamTF <elseban88@gmail.com>
2023-07-30 17:50:41 +0200

    Display card epic status

--- web-app/src/routes/card/[card]/+page.svelte
+++ web-app/src/routes/card/[card]/+page.svelte
@@ -61,7 +61,11 @@
                             <span class="card-number">#{data.card.number}</span>
                         </div>
 
-                        <h2 class="card-type">{data.card.subtype} {data.card.type}</h2>
+                        <h2 class="card-type">
+                            {data.card.epic ? 'Epic✦' : ''}
+                            {data.card.subtype} 
+                            {data.card.type}
+                        </h2>
                     </div>
 
                     <!-- Power -->
