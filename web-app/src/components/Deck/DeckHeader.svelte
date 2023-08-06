c6c35880c1ad58be0eb532e46808eff6344daa94
SamTF <elseban88@gmail.com>
2023-08-06 15:48:16 +0200

    Ability to bookmark, edit, remix, and delete deck!

--- web-app/src/components/Deck/DeckHeader.svelte
+++ web-app/src/components/Deck/DeckHeader.svelte
@@ -2,19 +2,25 @@
 <script>
     // Imports
     import { page } from '$app/stores'
-    import { currentUser } from '$lib/pocketbase.js'
+    import { currentUser, pb } from '$lib/pocketbase.js'
     import { decklistAdvance } from '$lib/stores/decklist'
     import { goto } from '$app/navigation'
-    import Icon from '../UI/Icon.svelte';
+    import Icon from '../UI/Icon.svelte'
+    import { openModal } from 'svelte-modals'
+    import Popup from '$components/UI/Popups/Popup.svelte'
     
     // Props
-    export let author, name, tags, authorID, deck, deckID
+    export let author, name, tags, authorID, fullCards
+    export let deck
 
     // whether this deck was created by the current authenticated user
     const userCreatedThisDeck = $currentUser ? authorID == $currentUser.id : false
 
-    const editDeck = () => {
-        decklistAdvance.set(deck)
+    // whether the user bookmarked this deck
+    $: deckIsBookmarked = $currentUser ? $currentUser.bookmarks.includes(deck.id) : ''
+
+    const remixDeck = () => {
+        decklistAdvance.set(fullCards)
         localStorage.setItem("decklist", JSON.stringify($decklistAdvance))
         localStorage.setItem("deckInfo", JSON.stringify({ 
             name: `${name} 2`,
@@ -22,7 +28,63 @@
             tags
         }))
 
-        goto(`/create/deck?edit=${deckID}`)
+        goto(`/create/deck?remix=${deck.id}`)
+    }
+
+    const editDeck = () => {
+        decklistAdvance.set(fullCards)
+        localStorage.setItem("decklist", JSON.stringify($decklistAdvance))
+        localStorage.setItem("deckInfo", JSON.stringify({ 
+            name,
+            author,
+            tags,
+            edit: true,
+            deckID: deck.id
+        }))
+
+        goto(`/create/deck?edit=${deck.id}`)
+    }
+
+    const deleteDeck = async () => {
+        try {
+            const data = await pb.collection('decks').delete(deck.id)
+            console.log(data)
+            openModal(Popup, { title: 'Deck successfully deleted', icon: 'sparkles' })
+            goto('/decks')
+        } catch (error) {
+            openModal(Popup, { title: 'Error x(', message: error })
+        }
+    }
+
+    const bookmarkDeck = async () => {
+        try {
+            const data = await pb.collection('users').getOne($currentUser.id)
+            console.log(data.bookmarks)
+            const bookmarks = [...data.bookmarks, deck.id]
+            console.log(bookmarks)
+            const update = await pb.collection('users').update($currentUser.id, {
+                bookmarks: bookmarks
+            })
+            console.log(data)
+            openModal(Popup, { title: 'Deck bookmarked', icon: 'sparkles' })
+        } catch (error) {
+            openModal(Popup, { title: 'Error x(', message: error })
+        }
+    }
+
+    const unbookmarkDeck = async () => {
+        try {
+            const index = $currentUser.bookmarks.indexOf(deck.id)
+            const bookmarks = $currentUser.bookmarks.filter(item => item !== deck.id)
+
+            const update = await pb.collection('users').update($currentUser.id, {
+                bookmarks: bookmarks
+            })
+
+            openModal(Popup, { title: 'Deck removed from bookmarks', icon: 'sparkles' })
+        } catch (error) {
+            openModal(Popup, { title: 'Error x(', message: error })
+        }
     }
 </script>
 
@@ -38,6 +100,12 @@
                     <li><a href={$page.url.pathname}>{tag}</a></li>
                 {/each}
             </ul>
+
+            {#if deck.remix}
+                <div class="remix">
+                    <p>Remix of <a href={`${deck.remix_of}`}>{deck.remix_of}</a></p>
+                </div>
+            {/if}
         </div>
 
         <!-- Special Controls for the deck author -->
@@ -46,17 +114,19 @@
                 <!-- Export as TXT -->
                 <button class="btn" title="Save decklist as Text file to import it somewhere else &#013;>>> Currently does nothing :)" disabled={true}>
                     <Icon name='export-file' class='header-btn-icon' strokeWidth='0' solid={true}/>
-                    <span>Export as TXT</span>
+                    <span>Export</span>
                 </button>
 
                 <!-- DELETE -->
-                <button class="btn" title="Delete this deck from the database! D:&#013;>>> Currently does nothing :)" disabled={true}>
+                <!-- <form action="?/delete" method="POST"> -->
+                <button class="btn" title="Delete this deck from the database. No take backsies!" on:click={deleteDeck}>
                     <Icon name='trash' class='header-btn-icon' strokeWidth='0' solid={true} />
                     <span>Delete Deck</span>
                 </button>
+                <!-- </form> -->
 
                 <!-- EDIT -->
-                <button class="btn" title=">>> Currently just remixes the deck (ie. it creates a copy instead of editing the original. I'm working on it!)" on:click={editDeck}>
+                <button class="btn" title="Update your deck and publish changes" on:click={editDeck}>
                     <Icon name='edit' class='header-btn-icon' strokeWidth='0' solid={true} />    
                     <span>Edit Deck</span>
                 </button>
@@ -66,26 +136,55 @@
                 <!-- Export as TXT -->
                 <button class="btn" title="Save decklist as Text file to import it somewhere else &#013;>>> Currently does nothing :)" disabled={true}>
                     <Icon name='export-file' class='header-btn-icon' strokeWidth='0' solid={true}/>
-                    <span>Export as TXT</span>
+                    <span>Export</span>
                 </button>
 
                 <!-- Remix -->
-                <button class="btn" on:click={editDeck} title="Edit this deck and create a new one!">
+                <button class="btn" on:click={remixDeck} title="Edit this deck and create a new one!">
                     <Icon name='remix' class='header-btn-icon' strokeWidth='2' />                         
                     <span>Remix</span>
                 </button>
 
                 <!-- Bookmark -->
-                <button class="btn" title="Bookmark this deck&#013;Must be logged in to user this feature" disabled={!$currentUser}>
-                    <Icon name='bookmark' class='header-btn-icon' strokeWidth='2' />
-                    <span>Bookmark</span>
-                </button>
+                {#key deckIsBookmarked}
+                {#if deckIsBookmarked}
+                    <button class="btn" title="Bookmark this deck&#013;Must be logged in to user this feature" disabled={!$currentUser} on:click={unbookmarkDeck}>
+                        <Icon name='bookmark' class='header-btn-icon' strokeWidth='2' solid={true} colour='gold'/>
+                        <span>Remove Bookmark</span>
+                    </button>
+                {:else}
+                    <button class="btn" title="Bookmark this deck&#013;Must be logged in to user this feature" disabled={!$currentUser} on:click={bookmarkDeck}>
+                        <Icon name='bookmark' class='header-btn-icon' strokeWidth='2' />
+                        <span>Bookmark</span>
+                    </button>
+                {/if}
+                {/key}
             {/if}
         </div>
     </div>
 </div>
 
-<div class="header-divider">
+<div class="header-divider"></div>
 
-</div>
+<style>
+    .remix {
+        margin-top: 1rem;
+        color: var(--colour-blue-dark);
+        font-weight: 700;
+        opacity: 0.75;
+    }
 
+    .remix a {
+        margin-left: 0.25rem;
+        padding: 0.25rem 0.5rem;
+
+        border-radius: 1rem;
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+
+    .remix a:hover {
+        background-color: var(--colour-blue-dark);
+        color: white;
+        opacity: 1;
+    }
+</style>
