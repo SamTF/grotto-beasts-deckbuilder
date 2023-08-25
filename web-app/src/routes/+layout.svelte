cdede51b1ef0672147a1aac73435a6f9d8afbe89
SamTF <elseban88@gmail.com>
2023-08-25 20:36:16 +0100

    Automatically log in user via cookies and token!

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -5,9 +5,40 @@
     import { decklistAdvance } from '$lib/stores/decklist'
     import { pb, currentUser } from '$lib/pocketbase.js'
     import { Modals, closeModal } from 'svelte-modals'
+    import { onMount } from 'svelte'
 
     const x = $decklistAdvance
     const y = $currentUser
+
+    // Check for User Auth Data cookies on mount
+    onMount(() => {
+        console.log('cookies', document.cookie)
+        
+        // check if there is an authData cookie
+        if (!document.cookie.includes("authData")) {
+            console.log("No user login cookies found!")
+            return
+        }
+        else {
+            console.log("User log in cookies found!")
+
+            // fetch the current logged in user (if any) via cookies!
+            const cookieValue = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("authData="))
+                ?.split("=")[1];
+
+            console.log('cookieValue', cookieValue)
+
+            const authData = JSON.parse(cookieValue);
+            console.log(authData)
+
+            if (!$currentUser) {
+                console.log("Not logged in!! Authenticating user automatically...")
+                pb.authStore.save(authData.token, { verified: true })
+            }
+        }
+    })
 </script>
 
 <Header />
