b4e04c47e1951b8e16ee1864ca45e627892dd2e1
SamTF <elseban88@gmail.com>
2023-11-06 13:20:39 +0100

    Auto show/hide mobile nav menu on scroll! :D

--- web-app/src/components/UI/Header.svelte
+++ web-app/src/components/UI/Header.svelte
@@ -21,9 +21,55 @@
             console.error(error)
         }
     }
+
+    // Y-scrolling
+    // code from: https://blog.kowalczyk.info/article/de13a71a392f488b9352b300b3ed722d/hide-header-on-scroll-in-svelte.html
+
+    let y = 0
+    let lastY = 0
+    let hideHeader = false;
+    $: headerClass = scrollAnimation(y)
+
+    // Update Y position and direction of scrolling
+    const scrollAnimation = (y) => {
+        const dy = lastY - y    // determine the direction of scrolling
+        lastY = y               // update last Y
+
+        return deriveClass(y, dy)
+    }
+
+    // hide when user scrolls down and show when user scrolls up.
+    const deriveClass = (y, dy) => {
+        const offset = 200
+        const threshold = 60
+
+        // Always true if at the top of the page
+        if (y < offset) {
+            return 'show'
+        }
+
+        // Scrolling down always hides below the offset
+        if (dy < 0) {
+            return 'hide'
+        }
+
+        // Check if the given threshold was surpassed or not
+        if (Math.abs(dy) <= threshold) {
+            return headerClass
+        }
+
+        // Check if scrolling up (with less tolerance)
+        if (dy > 0) {
+            return 'show'
+        }
+
+        // Otherwise, hide header
+        return 'hide'
+    }
 </script>
 
-<header>
+<!-- <header class:hide={hideHeader}> -->
+<header class={headerClass}>
     <div class="center">
         <a href="/">
             <img src="/images/logo.svg" alt="Grotto Beasts Logo" class="logo">
@@ -49,3 +95,5 @@
         </nav>
     </div>
 </header>
+
+<svelte:window bind:scrollY={y} />
