d0c01083c94aad621b47067b452cd8d04c6b6513
SamTF <elseban88@gmail.com>
2023-11-06 13:14:18 +0100

    Get card's image URL directly from its data object

--- web-app/src/components/Cards/Card.svelte
+++ web-app/src/components/Cards/Card.svelte
@@ -4,11 +4,19 @@
 <script>
     // Props
     export let card
+
+    const imageURL = `/images/cards/360/${card.number}. ${card.name}.webp`
 </script>
 
 <!-- HTML -->
 <div class="card" id={card.number}>
     <div class="card-image">
-        <a href={`/card/${card.number}`}><img src={`/images/cards/360/${card.number}. ${card.name}.webp`} alt={card.name} loading="lazy"></a>
+        <a href={`/card/${card.number}`}>
+            <img
+                src={card.imageURL.small || imageURL}
+                alt={card.name}
+                loading="lazy"
+            >
+        </a>
     </div> 
 </div>
