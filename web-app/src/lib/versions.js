393fa97d75a31553c1f9a08b9740751c09308ce0
SamTF <elseban88@gmail.com>
2023-11-06 13:11:52 +0100

    Keep track of grotto beast card versions (in prepartion for the TTS mod)

---
+++ web-app/src/lib/versions.js
@@ -0,0 +1,8 @@
+// Different Set Versions
+
+const cardVersions = [
+    { name: 'original', collection: 'cards', icon: '👴' },
+    { name: 'patched', collection: 'cards_patched', icon: '🤖' },
+]
+
+export default cardVersions
