a28d601329c738e247e471512708bcecff27b3b1
SamTF <elseban88@gmail.com>
2023-11-23 21:18:26 +0100

    Updated HTML structure to reflect new style

--- web-app/src/components/DeckExplorer/DeckGridItem.svelte
+++ web-app/src/components/DeckExplorer/DeckGridItem.svelte
@@ -11,5 +11,7 @@
 <a class="deck-grid-item" href={`/decks/${deck.id}`} style={`--bg-image: url("images/cards/360/${challengerImg}")`}>
     <h2 class="deck-item-name">{deck.name}</h2>
     <p class="deck-item-author">{deck.expand.author.username}</p>
-    <p class="deck-item-info">{deck.expand.challenger.name}</p>
+    <div class="deck-info-bottom">
+        <p class="deck-item-info">{deck.expand.challenger.name}</p>
+    </div>
 </a>
