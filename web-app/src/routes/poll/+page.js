ce7d4d7c29264f9ea25fcd93a05220b8dc5ff9b7
SamTF <elseban88@gmail.com>
2024-02-18 21:21:47 +0100

    Added route redirect to google form

---
+++ web-app/src/routes/poll/+page.js
@@ -0,0 +1,7 @@
+// Redirect to the google forms link
+
+import { json, error, redirect } from "@sveltejs/kit";
+
+export async function load({ params}) {
+    throw redirect(302, 'https://forms.gle/fNhiHXJLJ6iCo2LC9')
+}
