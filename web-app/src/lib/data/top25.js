b211c59e6a4eed0ea6554dd6d78589b25c1cd8fc
SamTF <elseban88@gmail.com>
2024-02-29 19:59:05 +0100

    Added voting results for round 2

---
+++ web-app/src/lib/data/top25.js
@@ -0,0 +1,355 @@
+const data = [
+    {
+        "title": "Alieums vs AiMbot-3940",
+        "data": [
+            {
+                "Option": "Alieums",
+                "Votes": 10
+            },
+            {
+                "Option": "AiMbot-3940",
+                "Votes": 22
+            }
+        ]
+    },
+    {
+        "title": "Baghost vs Beelt",
+        "data": [
+            {
+                "Option": "Baghost",
+                "Votes": 26
+            },
+            {
+                "Option": "Beelt",
+                "Votes": 6
+            }
+        ]
+    },
+    {
+        "title": "Biogator vs Birthday Basher",
+        "data": [
+            {
+                "Option": "Biogator",
+                "Votes": 14
+            },
+            {
+                "Option": "Birthday Basher",
+                "Votes": 19
+            }
+        ]
+    },
+    {
+        "title": "Bottled Nightmare vs Byeah Beast",
+        "data": [
+            {
+                "Option": "Bottled Nightmare",
+                "Votes": 8
+            },
+            {
+                "Option": "Byeah Beast",
+                "Votes": 25
+            }
+        ]
+    },
+    {
+        "title": "Connival vs Chromanova",
+        "data": [
+            {
+                "Option": "Connival",
+                "Votes": 15
+            },
+            {
+                "Option": "Chromanova",
+                "Votes": 17
+            }
+        ]
+    },
+    {
+        "title": "Dredgelord vs Dragossom",
+        "data": [
+            {
+                "Option": "Dredgelord",
+                "Votes": 13
+            },
+            {
+                "Option": "Dragossom",
+                "Votes": 19
+            }
+        ]
+    },
+    {
+        "title": "Dustbunny vs EEE-Vamp",
+        "data": [
+            {
+                "Option": "Dustbunny",
+                "Votes": 27
+            },
+            {
+                "Option": "EEE-Vamp",
+                "Votes": 12
+            }
+        ]
+    },
+    {
+        "title": "Festive Mimic vs Fretzel",
+        "data": [
+            {
+                "Option": "Festive Mimic",
+                "Votes": 13
+            },
+            {
+                "Option": "Fretzel",
+                "Votes": 26
+            }
+        ]
+    },
+    {
+        "title": "Gachatron vs Friendtriloquist",
+        "data": [
+            {
+                "Option": "Gachatron",
+                "Votes": 20
+            },
+            {
+                "Option": "Friendtriloquist",
+                "Votes": 12
+            }
+        ]
+    },
+    {
+        "title": "Googlow vs Harlefin",
+        "data": [
+            {
+                "Option": "Googlow",
+                "Votes": 21
+            },
+            {
+                "Option": "Harlefin",
+                "Votes": 17
+            }
+        ]
+    },
+    {
+        "title": "Hypodaze vs Highlyre",
+        "data": [
+            {
+                "Option": "Hypodaze",
+                "Votes": 25
+            },
+            {
+                "Option": "Highlyre",
+                "Votes": 8
+            }
+        ]
+    },
+    {
+        "title": "Jelly Monkey vs Jestapod",
+        "data": [
+            {
+                "Option": "Jelly Monkey",
+                "Votes": 13
+            },
+            {
+                "Option": "Jestapod",
+                "Votes": 19
+            }
+        ]
+    },
+    {
+        "title": "Jup the 1st vs Jup the 4th",
+        "data": [
+            {
+                "Option": "Jup the 1st",
+                "Votes": 16
+            },
+            {
+                "Option": "Jup the 4th",
+                "Votes": 23
+            }
+        ]
+    },
+    {
+        "title": "Lizard Wizard vs Lavabrys Shark",
+        "data": [
+            {
+                "Option": "Lizard Wizard",
+                "Votes": 26
+            },
+            {
+                "Option": "Lavabrys Shark",
+                "Votes": 7
+            }
+        ]
+    },
+    {
+        "title": "Meowdy vs Metranomatone",
+        "data": [
+            {
+                "Option": "Meowdy",
+                "Votes": 29
+            },
+            {
+                "Option": "Metranomatone",
+                "Votes": 4
+            }
+        ]
+    },
+    {
+        "title": "Moosaic vs Mr. Anycard",
+        "data": [
+            {
+                "Option": "Moosaic",
+                "Votes": 30
+            },
+            {
+                "Option": "Mr. Anycard",
+                "Votes": 9
+            }
+        ]
+    },
+    {
+        "title": "Nightmite vs Optimal Illusion",
+        "data": [
+            {
+                "Option": "Nightmite",
+                "Votes": 19
+            },
+            {
+                "Option": "Optimal Illusion",
+                "Votes": 20
+            }
+        ]
+    },
+    {
+        "title": "Sheetopillar vs Plunky",
+        "data": [
+            {
+                "Option": "Sheetopillar",
+                "Votes": 21
+            },
+            {
+                "Option": "Plunky",
+                "Votes": 13
+            }
+        ]
+    },
+    {
+        "title": "Peep-Peep vs Weeniemutt",
+        "data": [
+            {
+                "Option": "Peep-Peep",
+                "Votes": 6
+            },
+            {
+                "Option": "Weeniemutt",
+                "Votes": 33
+            }
+        ]
+    },
+    {
+        "title": "Protojammer vs Po\u00f1ata",
+        "data": [
+            {
+                "Option": "Protojammer",
+                "Votes": 29
+            },
+            {
+                "Option": "Po\u00f1ata",
+                "Votes": 10
+            }
+        ]
+    },
+    {
+        "title": "Razzleposs vs Puddle Puppy",
+        "data": [
+            {
+                "Option": "Razzleposs",
+                "Votes": 22
+            },
+            {
+                "Option": "Puddle Puppy",
+                "Votes": 11
+            }
+        ]
+    },
+    {
+        "title": "Seasprinter vs Rummage Rat",
+        "data": [
+            {
+                "Option": "Seasprinter",
+                "Votes": 4
+            },
+            {
+                "Option": "Rummage Rat",
+                "Votes": 28
+            }
+        ]
+    },
+    {
+        "title": "Sludge vs Snailsman",
+        "data": [
+            {
+                "Option": "Sludge",
+                "Votes": 24
+            },
+            {
+                "Option": "Snailsman",
+                "Votes": 8
+            }
+        ]
+    },
+    {
+        "title": "Small Enemy Spider vs Sneaky Rat",
+        "data": [
+            {
+                "Option": "Small Enemy Spider",
+                "Votes": 25
+            },
+            {
+                "Option": "Sneaky Rat",
+                "Votes": 8
+            }
+        ]
+    },
+    {
+        "title": "Spectocular vs Strong Rat",
+        "data": [
+            {
+                "Option": "Spectocular",
+                "Votes": 6
+            },
+            {
+                "Option": "Strong Rat",
+                "Votes": 33
+            }
+        ]
+    },
+    {
+        "title": "Thamacrow vs Sweet Tooth",
+        "data": [
+            {
+                "Option": "Thamacrow",
+                "Votes": 17
+            },
+            {
+                "Option": "Sweet Tooth",
+                "Votes": 16
+            }
+        ]
+    },
+    {
+        "title": "Toadstool vs Trickstircuit",
+        "data": [
+            {
+                "Option": "Toadstool",
+                "Votes": 20
+            },
+            {
+                "Option": "Trickstircuit",
+                "Votes": 18
+            }
+        ]
+    }
+]
+
+export default data
