7dc851df9f2207ad7c420397df169b2ce2775c68
SamTF <elseban88@gmail.com>
2024-03-02 20:23:01 +0100

    Updated poll link to Round 3

--- web-app/src/routes/poll/+page.js
+++ web-app/src/routes/poll/+page.js
@@ -3,5 +3,5 @@
 import { json, error, redirect } from "@sveltejs/kit";
 
 export async function load({ params}) {
-    throw redirect(302, 'https://forms.gle/Xh9gNbaffsRsoU3M7')
+    throw redirect(302, 'https://forms.gle/desFMDKDvPCVNPes5')
 }
