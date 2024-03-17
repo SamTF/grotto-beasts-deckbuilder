8eebce08b8d1be5378276f994e7ae3cd7151657b
SamTF <elseban88@gmail.com>
2024-03-17 12:19:01 +0100

    Added data results of the top 10 beasts poll

---
+++ web-app/src/lib/data/top10.js
@@ -0,0 +1,140 @@
+const data = [
+    {
+        "Beast": "Meowdy",
+        "Votes": 47,
+        "Percentage": "71.2%"
+    },
+    {
+        "Beast": "Sludge",
+        "Votes": 45,
+        "Percentage": "68.2%"
+    },
+    {
+        "Beast": "Fretzel",
+        "Votes": 39,
+        "Percentage": "59.1%"
+    },
+    {
+        "Beast": "Byeah Beast",
+        "Votes": 33,
+        "Percentage": "50.0%"
+    },
+    {
+        "Beast": "Moosaic",
+        "Votes": 32,
+        "Percentage": "48.5%"
+    },
+    {
+        "Beast": "Sheetopillar",
+        "Votes": 33,
+        "Percentage": "50.0%"
+    },
+    {
+        "Beast": "Small Enemy Spider",
+        "Votes": 33,
+        "Percentage": "50.0%"
+    },
+    {
+        "Beast": "Dustbunny",
+        "Votes": 30,
+        "Percentage": "45.5%"
+    },
+    {
+        "Beast": "Hypodaze",
+        "Votes": 29,
+        "Percentage": "43.9%"
+    },
+    {
+        "Beast": "Lizard Wizard",
+        "Votes": 29,
+        "Percentage": "43.9%"
+    },
+    {
+        "Beast": "Weeniemutt",
+        "Votes": 28,
+        "Percentage": "42.4%"
+    },
+    {
+        "Beast": "Optimal Illusion",
+        "Votes": 28,
+        "Percentage": "42.4%"
+    },
+    {
+        "Beast": "Razzleposs",
+        "Votes": 25,
+        "Percentage": "37.9%"
+    },
+    {
+        "Beast": "Baghost",
+        "Votes": 24,
+        "Percentage": "36.4%"
+    },
+    {
+        "Beast": "AiMbot-3940",
+        "Votes": 23,
+        "Percentage": "34.8%"
+    },
+    {
+        "Beast": "Strong Rat",
+        "Votes": 23,
+        "Percentage": "34.8%"
+    },
+    {
+        "Beast": "Jup the 4th",
+        "Votes": 20,
+        "Percentage": "30.3%"
+    },
+    {
+        "Beast": "Rummage Rat",
+        "Votes": 19,
+        "Percentage": "28.8%"
+    },
+    {
+        "Beast": "Thamacrow",
+        "Votes": 17,
+        "Percentage": "25.8%"
+    },
+    {
+        "Beast": "Chromanova",
+        "Votes": 16,
+        "Percentage": "24.2%"
+    },
+    {
+        "Beast": "Dragossom",
+        "Votes": 16,
+        "Percentage": "24.2%"
+    },
+    {
+        "Beast": "Protojammer",
+        "Votes": 16,
+        "Percentage": "24.2%"
+    },
+    {
+        "Beast": "Googlow",
+        "Votes": 15,
+        "Percentage": "22.7%"
+    },
+    {
+        "Beast": "Gachatron",
+        "Votes": 13,
+        "Percentage": "19.7%"
+    },
+    {
+        "Beast": "Jestapod",
+        "Votes": 12,
+        "Percentage": "18.2%"
+    },
+    {
+        "Beast": "Birthday Basher",
+        "Votes": 8,
+        "Percentage": "12.1%"
+    },
+    {
+        "Beast": "Toadstool",
+        "Votes": 7,
+        "Percentage": "10.6%"
+    }
+
+]
+
+export default data
