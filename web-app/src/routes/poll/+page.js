e2dd3fadb13d5303326e0f32f94ce10015b70ef3
SamTF <elseban88@gmail.com>
2024-03-17 12:22:09 +0100

    Poll link redirects to /iconic for now

--- web-app/src/routes/poll/+page.js
+++ web-app/src/routes/poll/+page.js
@@ -3,5 +3,5 @@
 import { json, error, redirect } from "@sveltejs/kit";
 
 export async function load({ params}) {
-    throw redirect(302, 'https://forms.gle/desFMDKDvPCVNPes5')
+    throw redirect(302, '/iconic')
 }
