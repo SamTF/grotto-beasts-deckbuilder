78dfbf407d97f2fe5af2754a12cbb42b864e9d45
SamTF <elseban88@gmail.com>
2024-03-17 12:20:09 +0100

    Parsing data for the top 10 page

---
+++ web-app/src/routes/iconic/top10/+page.server.js
@@ -0,0 +1,25 @@
+import grotto_data from "$lib/data/top10.js"
+
+export async function load({ event, url, params}) {
+    // Fetch local API data
+    const res = await fetch(`${url.origin}/api/cards/`)
+    let cards = await res.json()
+
+    console.log(grotto_data.length)
+
+    // loop over every beast in the voting results
+    for (let i = 0; i < grotto_data.length; i++) {
+        const beast = grotto_data[i]
+
+        // find beast card in database
+        const card = cards.original.find(c => c.name == beast.Beast)
+
+        // // add new data
+        grotto_data[i].imageURL = card ? card.imageURL : null
+        grotto_data[i].id = card ? card.number : null
+        grotto_data[i].name = beast.Beast
+        grotto_data[i].percentage = Math.round(parseFloat(beast.Percentage.replace('%', '')));
+    }
+
+    return {grotto_data}
+}
