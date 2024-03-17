d879e10e2678c86c0eab37544c9db664943ddeb6
SamTF <elseban88@gmail.com>
2024-03-17 12:20:38 +0100

    Created simple page break component

---
+++ web-app/src/components/Break.svelte
@@ -0,0 +1,22 @@
+<!-- A very simple Seperator to clearly seperate different topics or sections -->
+
+<!-- HTML -->
+<div class="page-break">
+    <span>✦</span>
+    <span>✦</span>
+    <span>✦</span>
+</div>
+
+<!-- CSS -->
+<style>
+    .page-break {
+        display: flex;
+        flex-direction: row;
+        gap: 3rem;
+
+        color: white;
+        font-size: 3rem;
+
+        margin-bottom: 2rem;
+    }
+</style>
