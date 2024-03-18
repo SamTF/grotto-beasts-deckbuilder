503bbfa4aae7c0ce56f1205c0c3965a99ea5566a
SamTF <elseban88@gmail.com>
2024-03-18 11:58:55 +0100

    Sort data array just in case

--- web-app/src/routes/iconic/top10/+page.server.js
+++ web-app/src/routes/iconic/top10/+page.server.js
@@ -5,7 +5,8 @@ export async function load({ event, url, params}) {
     const res = await fetch(`${url.origin}/api/cards/`)
     let cards = await res.json()
 
-    console.log(grotto_data.length)
+    // sort the array just in case it's messed up
+    grotto_data.sort((a, b) => b.Votes - a.Votes)
 
     // loop over every beast in the voting results
     for (let i = 0; i < grotto_data.length; i++) {
