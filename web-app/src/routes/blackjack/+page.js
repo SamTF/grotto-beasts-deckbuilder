a32c9760e37a2c5c51add23764f71e5897cb33ab
SamTF <elseban88@gmail.com>
2024-04-02 17:55:29 +0200

    Fetch deck and full cards

---
+++ web-app/src/routes/blackjack/+page.js
@@ -0,0 +1,38 @@
+import { pb } from '$lib/pocketbase.js'
+import { json, error, redirect } from "@sveltejs/kit"
+
+export async function load({ url, params}) {
+    // Load Deck data if a deck exists with the given ID
+    let deck = null
+    try {
+        const result = await pb.collection("decks").getOne('donfcktbwm5488s', {
+            expand: 'challenger, author'
+        })
+
+        deck = result
+    }
+    // If no deck was found with the given ID, redirect to deck explorer
+    catch (error) {
+        throw redirect(303, '/decks')        
+    }
+
+    // --- EXPAND CARD INFO --------
+    // get all cards from local API data
+    const res = await fetch(`${url.origin}/api/cards/`)
+    let allCards = await res.json()
+
+    // array of all card IDs in deck
+    const deckCardIds = Array.from(deck.cards_json.deck, x => x.id)
+
+    // filter all cards to only cards present in the deck
+    let fullCards = allCards.original.filter(x => deckCardIds.includes(x.number))
+
+    // Set correct quantity
+    for (let i = 0; i < fullCards.length; i++) {
+        const element = fullCards[i];
+        element.quantity = deck.cards_json.deck[i].quantity
+    }
+
+    // Return simplified deck and full expanded cards
+    return { deck: deck, fullCards: fullCards }
+}
