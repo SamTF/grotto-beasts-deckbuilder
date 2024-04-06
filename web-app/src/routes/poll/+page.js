a478e6912715dd4f68449a99096b85fd2ee7514c
SamTF <elseban88@gmail.com>
2024-04-06 12:47:04 +0200

    Updated poll link to Top 10 Challengers

--- web-app/src/routes/poll/+page.js
+++ web-app/src/routes/poll/+page.js
@@ -3,5 +3,5 @@
 import { json, error, redirect } from "@sveltejs/kit";
 
 export async function load({ params}) {
-    throw redirect(302, '/iconic')
+    throw redirect(302, 'https://forms.gle/4STv6wa6xwu6MgYV6')
 }
