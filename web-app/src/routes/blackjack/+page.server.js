872420f6a0733ed8d025502bfc65aa9f4981d308
SamTF <elseban88@gmail.com>
2024-05-06 13:17:03 +0200

    Changed API to serverside only

--- web-app/src/routes/blackjack/+page.server.js
+++ web-app/src/routes/blackjack/+page.server.js
@@ -1,7 +1,9 @@
 import { pb } from '$lib/pocketbase.js'
 import { json, error, redirect } from "@sveltejs/kit"
 
-export async function load({ url, params}) {
+export async function load({ url, fetch, params, setHeaders}) {
+    console.log(url.origin)
+
     // Load Deck data if a deck exists with the given ID
     let deck = null
     try {
@@ -18,7 +20,8 @@ export async function load({ url, params}) {
 
     // --- EXPAND CARD INFO --------
     // get all cards from local API data
-    const res = await fetch(`${url.origin}/api/cards/`)
+    // const res = await fetch(`${url.origin}/api/cards/`)
+    const res = await fetch(`https://grotto.builders/api/cards/`)
     let allCards = await res.json()
 
     // array of all card IDs in deck
@@ -33,6 +36,12 @@ export async function load({ url, params}) {
         element.quantity = deck.cards_json.deck[i].quantity
     }
 
+    // Choose a random challenger
+    const challengers = allCards.original.filter(x => x.type == 'Challenger')
+
     // Return simplified deck and full expanded cards
-    return { deck: deck, fullCards: fullCards }
+    // return { deck: deck, fullCards: fullCards, challengers: challengers }
+
+    // Return serialized data
+    return { deck: structuredClone(deck), fullCards: structuredClone(fullCards), challengers: structuredClone(challengers) }
 }
