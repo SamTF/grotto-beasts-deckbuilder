1a2bac9f67eb49d1a0601066f629779af715a760
SamTF <elseban88@gmail.com>
2024-05-17 00:30:56 +0200

    Random assortment of utility functions used only in the Blackjack game

---
+++ web-app/src/lib/blackjack/utils.js
@@ -0,0 +1,27 @@
+// Random assortment of utility functions used only in the Blackjack game
+
+// Find the index of the last played Beast
+export const findLastBeastIndex = (cards) => {
+    for (let i = cards.length - 1; i >= 0; i--) {
+        if (cards[i].type === "Beast") {
+            return i
+        }
+    }
+    
+    // If no Beast is found, return -1
+    return -1
+}
+
+// Finds the INDEX of all beasts
+export const findBeastIds = (cards) => {
+    const indices = []
+
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].type == 'Beast') {
+            indices.push(i)
+        }
+        
+    }
+
+    return indices
+}
