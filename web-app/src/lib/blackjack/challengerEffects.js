d486414b515facd1abbc809184a7873108852136
SamTF <elseban88@gmail.com>
2024-05-17 00:34:03 +0200

    File storing challenger effects

---
+++ web-app/src/lib/blackjack/challengerEffects.js
@@ -0,0 +1,9 @@
+const challengerEffects = {
+    1: "Only the last Beast played counts for Score.\n(other played Beasts still add Mult)",
+    2: "All cards debuffed until 3 cards discarded",
+    3: "-2 hand size",
+    4: "Discards all Beasts in hand after Scoring",
+    5: "You have no Discards"
+}
+
+export default challengerEffects
