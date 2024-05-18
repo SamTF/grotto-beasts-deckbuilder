150d4ff84ab2e317e3f26fb5fc367b121651d9fd
SamTF <elseban88@gmail.com>
2024-05-18 15:47:02 +0200

    Added new effects

--- web-app/src/lib/blackjack/challengerEffects.js
+++ web-app/src/lib/blackjack/challengerEffects.js
@@ -3,7 +3,9 @@ const challengerEffects = {
     2: "All cards debuffed until 3 cards discarded",
     3: "-2 hand size",
     4: "Discards all Beasts in hand after Scoring",
-    5: "You have no Discards"
+    5: "You have no Discards",
+    6: "Goal doubles after every hand",
+    7: "All Wishes become either Negative or Doubled",
 }
 
 export default challengerEffects
