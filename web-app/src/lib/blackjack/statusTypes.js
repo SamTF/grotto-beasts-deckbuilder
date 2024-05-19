9abbcf7c5280962468306f57bd80536f3aaaf80a
SamTF <elseban88@gmail.com>
2024-05-19 15:52:21 +0200

    Enum to hold all Card Status Types

---
+++ web-app/src/lib/blackjack/statusTypes.js
@@ -0,0 +1,9 @@
+// Eenum of Card Status effects in Mr Greenz Blackjack
+const StatusTypes = {
+    DEBUFFED : 'Debuffed',
+    HALF_DEBUFFED : 'Half-Debuffed',
+    DOUBLE : 'Double',
+    NEGATIVE : 'Negative'
+}
+
+export default StatusTypes
