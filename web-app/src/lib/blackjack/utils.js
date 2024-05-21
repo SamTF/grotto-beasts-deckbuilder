c53a4867af1a634ee990e666ac56b73bd8d72f0c
SamTF <elseban88@gmail.com>
2024-05-21 15:29:41 +0200

    Added populateDeck func to utils

--- web-app/src/lib/blackjack/utils.js
+++ web-app/src/lib/blackjack/utils.js
@@ -2,11 +2,39 @@
 
 // Imports
 import StatusTypes from "$lib/blackjack/statusTypes.js"
+import { CardTypes } from "$lib/cardUtils"
+import { nanoid } from 'nanoid'
+
+// Create a deck from the given cards and their respective quantity
+export const populateDeck = (cards) => {
+    let deck = []
+
+    // loop over all cards and append them to the deck according to their quantity
+    cards.forEach(c => {
+        // ignore challengers
+        if (c.type == CardTypes.CHALLENGER) {
+            return
+        }
+
+        // append cards to deck
+        for (let i = 0; i < c.quantity; i++) {
+            // Create a new simplified card Object with a UUID
+            const { number, name, type, imageURL, cost, power } = c
+            const newCard = { number, name, type, imageURL, cost, power, id: nanoid(), status: 'normal' }
+            
+            // add card to deck
+            deck.push(newCard)
+        }
+    })
+
+    // return the populated deck
+    return deck
+}
 
 // Find the index of the last played Beast
 export const findLastBeastIndex = (cards) => {
     for (let i = cards.length - 1; i >= 0; i--) {
-        if (cards[i].type === "Beast") {
+        if (cards[i].type === CardTypes.BEAST) {
             return i
         }
     }
@@ -20,7 +48,7 @@ export const findBeastIds = (cards) => {
     const indices = []
 
     for (let i = 0; i < cards.length; i++) {
-        if (cards[i].type == 'Beast') {
+        if (cards[i].type == CardTypes.BEAST) {
             indices.push(i)
         }
         
