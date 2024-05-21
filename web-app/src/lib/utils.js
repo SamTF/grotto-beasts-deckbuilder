8736dd170da4672fcaabdd062775aa26909e3ad7
SamTF <elseban88@gmail.com>
2024-05-21 15:29:25 +0200

    Created function to deep clone array of objects

--- web-app/src/lib/utils.js
+++ web-app/src/lib/utils.js
@@ -38,4 +38,26 @@ export const getRandomItem = array => {
 // Wait X MS before executing rest of code
 export const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
+
+// Deep clone array of nested Objects
+export const deepClone = obj => {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    // Handle Array
+    if (Array.isArray(obj)) {
+        return obj.map(deepClone);
+    }
+
+    // Handle Object
+    const clonedObj = {}
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clonedObj[key] = deepClone(obj[key])
+        }
+    }
+
+    return clonedObj
+}
